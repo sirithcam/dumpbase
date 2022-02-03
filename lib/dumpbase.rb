d85362eb8bc1ca3c9192ce6d7738b3cb06824835
Mateusz Czuchryta <mateusz.czuchryta@gmail.com>
2022-02-03 22:03:33 +0100

    init commit

---
+++ lib/dumpbase.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dumpbase/version"
+
+module Dumpbase
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dumpbase/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Dumpbase
+  VERSION = "0.1.0"
+end
